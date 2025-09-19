97b138b03df9248deb377204c9293c46f796cea9
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-09-19 16:48:33 +0330

    add logging to telegram

--- telegram/tasks.py
+++ telegram/tasks.py
@@ -1,13 +1,17 @@
 import os
 import pyrogram
+import logging
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID")
 TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")
 
+logger = logging.Logger("Telegram", 20)
 
 app = pyrogram.Client("bot", bot_token=TELEGRAM_BOT_TOKEN, api_hash=TELEGRAM_API_HASH, api_id=TELEGRAM_API_ID)
 
 @app.on_message(pyrogram.filters.command("start"))
 def handle_notification(client, message):
     message.reply_text("سلام من دستیار هوشمند بانک گردشگری هستم. چطور می‌تونم کمکتون کنم؟")
+logger.info("Starting App...")
+app.start()
