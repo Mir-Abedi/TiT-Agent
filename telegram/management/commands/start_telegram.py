5c04840fbe6e84b2cdc1c5f509e3c18fe003d3a7
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-09-30 19:41:48 +0330

    Add envs

--- telegram/management/commands/start_telegram.py
+++ telegram/management/commands/start_telegram.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
+import os
 from telegram.tasks import get_telegram_app
 
 class Command(BaseCommand):
     help = 'Starts telegram bot.'
 
     def handle(self, *args, **options):
+        print(os.getenv("TELEGRAM_BOT_TOKEN"))
         get_telegram_app().run()
