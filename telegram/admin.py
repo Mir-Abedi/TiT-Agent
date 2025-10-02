c8f8bc1240e0e6de4cd993e934d69fe02c2e0098
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-10-02 12:39:30 +0330

    Add admin

--- telegram/admin.py
+++ telegram/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from telegram.models import UserMessage, BotMessage
 
-# Register your models here.
+class UserMessageAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'chat_id', 'text', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user_id', 'chat_id', 'text')
+
+admin.site.register(UserMessage, UserMessageAdmin)
+
+class BotMessageAdmin(admin.ModelAdmin):
+    list_display = ('user_message_id', 'text', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user_message_id', 'text')
+
+admin.site.register(BotMessage, BotMessageAdmin)
