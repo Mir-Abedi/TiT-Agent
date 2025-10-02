8f367d2e5b6070d14b20b09e89ddf2f7a9fa1cf5
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-10-02 18:54:28 +0330

    Add docks and FAQ

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    category = models.TextField()
+    sub_category = models.TextField()
+    solution = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_at"]
+
+class FAQ(models.Model):
+    category = models.TextField()
+    question = models.TextField()
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_at"]
