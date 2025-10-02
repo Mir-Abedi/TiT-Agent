4e68b2ff816051bde3d01bea7bbef8f6ea1edec9
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-10-02 12:43:42 +0330

    Fix admin

--- telegram/admin.py
+++ telegram/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from telegram.models import UserMessage, BotMessage
 
 class UserMessageAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'chat_id', 'text', 'created_at')
-    list_filter = ('created_at',)
+    list_display = ('user_id', 'chat_id', 'text', 'timestamp')
+    fields = ('user_id', 'chat_id', 'text', 'timestamp', 'bot_message')
     search_fields = ('user_id', 'chat_id', 'text')
 
 admin.site.register(UserMessage, UserMessageAdmin)
 
 class BotMessageAdmin(admin.ModelAdmin):
-    list_display = ('user_message_id', 'text', 'created_at')
-    list_filter = ('created_at',)
-    search_fields = ('user_message_id', 'text')
+    list_display = ('user_message', 'text', 'timestamp')
+    fields = ('user_message', 'text', 'timestamp')
+    search_fields = ('user_message', 'text')
 
 admin.site.register(BotMessage, BotMessageAdmin)
