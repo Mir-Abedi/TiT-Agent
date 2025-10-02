013560395538240d5142222e44028799f157b49a
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-10-03 03:02:10 +0330

    Add fast model to settings

--- tit/settings.py
+++ tit/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from chatbot.tasks import API_FAST_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -143,6 +145,7 @@ TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_ENDPOINT = os.getenv("OPENAI_ENDPOINT")
 API_MODEL = os.getenv("API_MODEL", "GPT-4.1-Mini")
+API_FAST_MODEL = os.getenv("API_FAST_MODEL", "Qwen3-30B-A3B")
 
 DJANGO_ALLOW_ASYNC_UNSAFE = True
 
