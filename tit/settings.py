a8fc4b70215ba1898fef0e76ac346c91fc0e5ff7
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-10-02 12:49:38 +0330

    Fix db

--- tit/settings.py
+++ tit/settings.py
@@ -75,14 +75,21 @@ WSGI_APPLICATION = 'tit.wsgi.application'
 
 import os
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('POSTGRES_DB', 'mydb'),
+#         'USER': os.getenv('POSTGRES_USER', 'myuser'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'mypassword'),
+#         'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+#         'PORT': os.getenv('POSTGRES_PORT', '5432'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'mydb'),
-        'USER': os.getenv('POSTGRES_USER', 'myuser'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'mypassword'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
