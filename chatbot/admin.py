a380aac8c16fec41ff7859d8366477c3199aa54a
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-10-02 18:56:42 +0330

    Add docs admin

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from chatbot.models import Document, FAQ
 
-# Register your models here.
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('category', 'sub_category', 'solution')
+    fields = ('category', 'sub_category', 'solution')
+    search_fields = ('category', 'sub_category', 'solution')
+
+admin.site.register(Document, DocumentAdmin)
+
+
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('category', 'question', 'answer')
+    fields = ('category', 'question', 'answer')
+    search_fields = ('category', 'question', 'answer')
+
+admin.site.register(FAQ, FAQAdmin)
