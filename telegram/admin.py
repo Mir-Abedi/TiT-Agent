38d15ccedb4aafc930ff32451479eb9419a1223b
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-10-02 13:05:39 +0330

    Fix admin

--- telegram/admin.py
+++ telegram/admin.py
@@ -4,6 +4,7 @@ from telegram.models import UserMessage, BotMessage
 class UserMessageAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'chat_id', 'text', 'timestamp')
     fields = ('user_id', 'chat_id', 'text', 'timestamp', 'bot_message')
+    readonly_fields = fields
     search_fields = ('user_id', 'chat_id', 'text')
 
 admin.site.register(UserMessage, UserMessageAdmin)
@@ -11,6 +12,7 @@ admin.site.register(UserMessage, UserMessageAdmin)
 class BotMessageAdmin(admin.ModelAdmin):
     list_display = ('user_message', 'text', 'timestamp')
     fields = ('user_message', 'text', 'timestamp')
+    readonly_fields = fields
     search_fields = ('user_message', 'text')
 
 admin.site.register(BotMessage, BotMessageAdmin)
